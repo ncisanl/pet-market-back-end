4eb6f768fcb12812a987690153fda253f93367d1
Nicolás San Martin <nicolas.sm.lor@gmail.com>
2025-04-02 12:20:37 -0300

    implementacion de test para regiones y comunas, ademas, implementacion de un correcto manejo de errores

--- controllers/petmarket.controller.js
+++ controllers/petmarket.controller.js
@@ -2,14 +2,19 @@ import "dotenv/config";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { petMarketModel } from "../models/petmarket.model.js";
+import { getDatabaseError } from "../lib/errors/database.error.js";
 
 const getRegionsController = async (req, res) => {
   try {
-    const regiones = await petMarketModel.getRegionsModel();
+    const regions = await petMarketModel.getRegionsModel();
 
-    return res.status(200).json(regiones);
+    return res.status(200).json(regions);
   } catch (error) {
     console.log(error);
+    if (error.code) {
+      const { code, message } = getDatabaseError(error.code);
+      return res.status(code).json({ message });
+    }
     return res.status(500).json({ message: "Internal Server Error" });
   }
 };
@@ -23,6 +28,10 @@ const getCommunesController = async (req, res) => {
     return res.status(200).json(communes);
   } catch (error) {
     console.log(error);
+    if (error.code) {
+      const { code, message } = getDatabaseError(error.code);
+      return res.status(code).json({ message });
+    }
     return res.status(500).json({ message: "Internal Server Error" });
   }
 };
--- test/petmarket.spec.js
+++ test/petmarket.spec.js
@@ -1,2 +1,21 @@
-import { request } from "supertest";
+import request from "supertest";
 import { app } from "../index";
+
+describe("GET - /regions", () => {
+  it("status code 200", async () => {
+    const response = await request(app).get("/pet-market/regions");
+    expect(response.statusCode).toBe(200);
+  });
+});
+
+describe("GET - /communes/:regionId", () => {
+  it("status code 200", async () => {
+    const response = await request(app).get("/pet-market/communes/1");
+    expect(response.statusCode).toBe(200);
+  });
+
+  it("status code 400 and message invalid params value", async () => {
+    const response = await request(app).get("/pet-market/communes/a");
+    expect(response.statusCode).toBe(400);
+  });
+});
