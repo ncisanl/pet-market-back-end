27f7eaef1de5f8aa6eb29fd46b6465aad73f667d
Nicolás San Martin <nicolas.sm.lor@gmail.com>
2025-04-10 15:47:30 -0400

    implementacion de configuraciones para utilizar BBDD desplegada

--- database/connection.js
+++ database/connection.js
@@ -4,10 +4,15 @@ import "dotenv/config";
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
-  allowExitOnIdle: true,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  // TODO: Descomentar para la conexion local
+  // host: process.env.PGHOST,
+  // port: process.env.PGPORT,
+  // user: process.env.PGUSER,
+  // password: process.env.PGPASSWORD,
+  // database: process.env.PGDATABASE,
+  // allowExitOnIdle: true,
 });
